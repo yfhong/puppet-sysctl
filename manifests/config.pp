b0ed9ae3e64de049219cb2e13012a1687ad30cd5
Hong Yifan <hyfing@gmail.com>
2015-06-26 10:36:53 +0800

    Bugfix, duplicatied type define

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,15 +10,8 @@ class sysctl::config {
 
   $entries = hiera_hash('sysctl::entries', undef)
 
-  $system_defaults = {
-    ensure => absent,
-    target => "${conffile}",
-    apply  => false,
-  }
-
   $custom_defaults = {
     ensure => present,
-    target => "${customized_file}",
     apply  => true,
   }
 
@@ -32,9 +25,6 @@ class sysctl::config {
   }
 
   if ($entries) {
-    # Customized kernel parameter always be removed from system default settings.
-    create_resources(sysctl, $entries, $system_defaults)
-
     # Setup customized parameter
     create_resources(sysctl, $entries, $custom_defaults)
   }
